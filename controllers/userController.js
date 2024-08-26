dff10e61c0dd4562ee85098d0c24957cc5059e57
hemangprashar18 <hemangprashar18@github.com>
2024-08-26 14:57:44 +0530

    modified user details

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,9 @@ const User = require('../models/userModel');
 exports.getUserProfile = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).json({ message: 'User not found' });
+    if (!user) {
+     return res.status(404).json({ message: 'User not found' });
+    }  
 
     res.status(200).json(user);
   } catch (error) {
@@ -16,11 +18,13 @@ exports.getUserProfile = async (req, res) => {
 exports.addUserBio = async (req, res) => {
   try {
     const { bio } = req.body;
-    if (bio.length > 500) return res.status(400).json({ message: 'Bio exceeds 500 characters' });
-
+    if (bio.length > 500) {
+      return res.status(400).json({ message: 'Bio exceeds 500 characters' });
+    }
     const user = await User.findByIdAndUpdate(req.params.id, { bio }, { new: true });
-    if (!user) return res.status(404).json({ message: 'User not found' });
-
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
